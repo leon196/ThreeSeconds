6c82c9b496080251a596624151ff02b7aee73ab5
Leon Denise <lowpolygonum@gmail.com>
2016-04-24 19:44:21 +0200

    omagad

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	float speed = 1f;
+
+	void Start () 
+	{	
+	}
+	
+	void Update () 
+	{
+		float h = Input.GetAxis("Mouse X");
+		float v = Input.GetAxis("Mouse Y");
+
+		transform.Rotate(Vector3.up * h * Time.deltaTime);
+		transform.Rotate(transform.right * h * Time.deltaTime);
+
+		transform.Translate(transform.forward * speed * Time.deltaTime);
+	}
+}
